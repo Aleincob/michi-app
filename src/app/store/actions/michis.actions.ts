f77ae6542af7024e79168d546128deac2dde21c9
Alejandro <alecaperez13@gmail.com>
2023-01-26 23:26:21 -0500

    added new actions

--- src/app/store/actions/michis.actions.ts
+++ src/app/store/actions/michis.actions.ts
@@ -1,4 +1,12 @@
 import { createAction, props } from '@ngrx/store';
 import { Michi } from 'src/app/features/models/Michi';
 
-export const addMichi = createAction('[Register] addMichi', props<{ newMichi: Michi }>())
+export const addMichi = createAction(
+  '[Michis] addMichi',
+  props<{ newMichi: Michi }>()
+);
+export const loadMichis = createAction('[Michis] loadMichis');
+export const setMichis = createAction(
+  '[Michis] setMichis',
+  props<{ michis: Michi[] }>()
+);
--- src/app/store/reducers/michis.reducer.ts
+++ src/app/store/reducers/michis.reducer.ts
@@ -1,10 +1,11 @@
-import { createReducer, on } from "@ngrx/store";
-import { addMichi } from '../actions/michis.actions';
-import { Michi } from "src/app/features/models/Michi";
+import { createReducer, on } from '@ngrx/store';
+import { addMichi, loadMichis, setMichis } from '../actions/michis.actions';
+import { Michi } from 'src/app/features/models/Michi';
 
-export const initialState: Michi[] = []
+export const initialState: Michi[] = [];
 
 export const michisReducer = createReducer(
-    initialState,
-    on(addMichi, (state, { newMichi }) => [...state, newMichi]),
-)
+  initialState,
+  on(addMichi, (state, { newMichi }) => [...state, newMichi]),
+  on(setMichis, (state, { michis }) => [...state, ...michis])
+);
